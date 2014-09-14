b444576f1b590de0d555caaadf1d89d19961c70e
jobayerccj <jobayerccj@users.noreply.github.com>
2014-09-14 23:06:42 +0600

    Update delete_campaign.php

--- jobayer/admin/users/delete_campaign.php
+++ jobayer/admin/users/delete_campaign.php
@@ -37,7 +37,7 @@ if(isset($_GET['id'])){
 
 $id = $_GET['id'];
 //echo 'test';
-$dbh = new PDO('mysql:host=localhost;dbname=vpndb', 'vpndb', 'ENFEqax3VGdQmC6t');
+$dbh = new PDO('mysql:host=localhost;dbname=database_name', 'username', 'password');
 $stmt = $dbh->prepare("DELETE FROM campaign WHERE id='$id'");
 $stmt->execute() ;
 echo "<script>
@@ -64,18 +64,18 @@ echo "<script>
 				if (isset($_GET["page"])) { 
 				
 				$page = $_GET["page"]; 
-				//echo $page;
+			
 				} 
 				else { $page=1; };
 				$start_from = ($page-1) * 10; 
 				
-				$dbh = new PDO('mysql:host=localhost;dbname=vpndb', 'vpndb', 'ENFEqax3VGdQmC6t');
+				$dbh = new PDO('mysql:host=localhost;dbname=database_name', 'username', 'password');
 				$stmt = $dbh->prepare("SELECT * FROM campaign LIMIT $start_from, 10");
 				$stmt->execute() ;
 				
-				//$result = $sth->fetch(PDO::FETCH_ASSOC);
+				
 				$rows = $stmt->fetchAll() ;
-				//$usersdata = mysql_query("SELECT * FROM `campaign`");
+				
 				foreach($rows as $userdetails)
 				{
 					?>
@@ -86,7 +86,7 @@ echo "<script>
 			<div class="user_name" style="width:60%;"><?php echo $userdetails['formatted_text'];?></div>
 			<div class="first_name"><img src="<?php echo $userdetails['logo'];?>" alt="logo" width="50%"/></div>
 			<div class="last_name"><a href="#" ONCLICK="if(confirm('Are you sure!!!')) {loadWholePage('delete_campaign.php?id=<?php echo $userdetails['id'];?>');}">Delete</a></div>
-			<!--loadWholePage('delete_campaign.php?id=<?php echo $userdetails['id'];?>')-->
+			
 		</div>
 <?php
 
@@ -98,7 +98,7 @@ echo "<script>
 				<div id="pagination" style="">
 				
 				<?php
-				//echo 'testing pagination';
+				
 				
 				$result = $dbh->prepare("SELECT COUNT(id) FROM campaign");
 				$result->execute();
@@ -107,16 +107,7 @@ echo "<script>
 				
 				$total_pages = ceil($total_records / 10);
 				 
-				/*for ($i=1; $i<=$total_pages; $i++) {
 				
-				echo "<a href='view-logs2.php?page=".$i."'";
-				if($page==$i)
-				{
-				echo "id=active";
-				}
-				echo ">";
-				echo "".$i."</a> ";
-				}*/
 				
 				
 				if($total_pages>1){
@@ -137,105 +128,6 @@ header('Location:../index.php');
  } ?>
 
 
-
-<!-- For validations -->
-<script src="http://<?php echo $jqueryminjs; ?>"></script>
-
-<script src="http://<?php echo $validateminjs; ?>"></script>
-
-<!-- validation end --> 
-
-<!-- jQuery Form Validation code -->
-<script>
-$(document).ready(function(){
-	jQuery.validator.addMethod("noSpace", function(value, element)
-    	{ return value.indexOf(" ") < 0; }, "No space in Password");
-    	$.validator.addMethod("alpha", function(value, element) {
-    return this.optional(element) || value == value.match(/^[a-zA-Z ]*$/);
- });
-    $("#regform").validate({
-    
-        // Specify the validation rules
-        rules: {
-            uname: {
-				required:true,
-				minlength:5,
-				noSpace: true
-				},
-            email: {
-                required: true,
-                email: true
-            },
-            upassword: {
-                required: true,
-                minlength: 5,
-                noSpace: true
-            },
-            confirm_password:{
-					required:true,
-					equalTo:"#uppassword"
-				},
-            fname:{
-				required: true,
-				minlength: 3,
-				alpha: true
-				},
-			lname:{
-				required:true,
-				minlength: 3,
-				alpha: true
-				},
-			designation:{
-				required:true,
-				minlength:4
-				},
-			empno:{
-				required: true,
-				},
-			organisation:{
-				required:true,
-				},
-				uemail:{
-					required:true,
-					}
-				
-        },
-        
-        // Specify the validation error messages
-        errorElement: "span",
-        messages: {
-            uname: {
-				required: "Please choose Username",
-				noSpace: "Spaces are not allowed in Username"
-			},
-            fname:{
-				required: "Please Enter your Name",
-				alpha: "Only Characters are allowed"
-			},
-            lname:{
-				required: "Please enter your Last Name",
-				alpha: "Only Characters are allowed"
-			},
-            designation:"Field is required",
-            empno: "Field is required",
-            confirm_password:"Password don not Match",
-            organisation:"Field is required",
-            uemail: "Please enter a valid email address",
-            username: "Please enter a valid username",
-            password: {
-                required: "Please provide a password",
-                minlength: "Your password must be at least 5 characters long",
-                noSpace: "Spaces are not allowed in Password"
-            }
-        },
-        
-        submitHandler: function(form) {
-            form.submit();
-        }
-    });
-
-  });
-</script>
 
 </div>
 	
